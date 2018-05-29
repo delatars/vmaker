c70b32511d0a393487e43130c5f1f7d85d30f68e
Alexandr Morokov <a.morokov@drweb.com>
2018-05-29 12:07:01 +0300

    fixes

--- vmaker/utils/auxilary.py
+++ vmaker/utils/auxilary.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 from time import time
 
 
@@ -20,12 +19,6 @@ def timer(function):
         print "Function spent: %s" % res
         return True
     return wrapper
-
-
-with open(os.path.join("box.ovf"), "r") as ovf:
-    ovf_file = ovf.read()
-with open(os.path.join("box.ovf"), "w") as ovf:
-    ovf.write(ovf_file.replace("centos6-amd64-disk001.vmdk", "box-disk.vmdk"))
 
 
 if __name__ == "__main__":
